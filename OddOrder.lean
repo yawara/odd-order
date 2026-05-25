ef0bd99dcefa565297357389d4cf7a16c63ac994
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-05-25 14:42:55 +0900

    BG s03: start Frobenius actions

--- OddOrder.lean
+++ OddOrder.lean
@@ -21,4 +21,5 @@ import OddOrder.Isaacs.Ch04_Commutators.ForwardFromCh03
 import OddOrder.Isaacs.Ch04_Commutators.Main
 import OddOrder.Isaacs.Ch07_ThompsonSubgroup.ForwardFromCh03
 import OddOrder.BG.Ch1_Preliminary.S01_Solvable
+import OddOrder.BG.Ch1_Preliminary.S03_FrobeniusActions
 import OddOrder.AxiomsCheck
