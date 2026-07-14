817dbddd1275d571d06da8f79e98bc1f2d8726ed
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-07-15 02:19:20 +0900

    feat(peterfalvi): realize the S-side field model

--- OddOrder/Peterfalvi/S15_SAndT.lean
+++ OddOrder/Peterfalvi/S15_SAndT.lean
@@ -8,6 +8,7 @@ import OddOrder.Peterfalvi.S15_CaseASylowCenter
 import OddOrder.Peterfalvi.S15_CaseAOmegaCenter
 import OddOrder.Peterfalvi.S15_CaseAOmegaFixedPointFree
 import OddOrder.Peterfalvi.S15_CaseAContradiction
+import OddOrder.Peterfalvi.S15_SSideGaloisFieldModel
 import OddOrder.Peterfalvi.S16_PairingCoherence
 import OddOrder.Peterfalvi.S16_PairingBessel
 import OddOrder.Peterfalvi.S16_GridExpansion
