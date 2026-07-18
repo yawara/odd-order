257ae19a88c53de8dfc9752b6be0c8d2c4a4b9d7
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-07-18 09:44:55 +0900

    chore(isaacs-app): Isaacs Appendix DirectDiamond を root import

--- OddOrder.lean
+++ OddOrder.lean
@@ -154,6 +154,7 @@ import OddOrder.Isaacs.Ch08_PermutationGroups.Subdegrees
 import OddOrder.Isaacs.Ch08_PermutationGroups.CommonDivisorGraph
 import OddOrder.Isaacs.Ch10_MoreTransfer.HuppertMetacyclic
 import OddOrder.Isaacs.Ch10_MoreTransfer.Main
+import OddOrder.Isaacs.Appendix.DirectDiamond
 import OddOrder.BG.Ch1_Preliminary.S01_Solvable
 import OddOrder.BG.Ch1_Preliminary.S01c_Omega1Rigidity
 import OddOrder.BG.Ch1_Preliminary.S02_Representations
