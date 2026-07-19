accc968163e1c4db87df20d434028c64679cc1ce
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-07-19 13:18:13 +0900

    feat(peterfalvi): construct Higman lower-central layers

--- OddOrder/Peterfalvi/Appendices/Suzuki2Groups.lean
+++ OddOrder/Peterfalvi/Appendices/Suzuki2Groups.lean
@@ -17,6 +17,7 @@ import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanIdempotentCovariance
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanKernel
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanImageOrder
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanFinalCase
+import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanLowerCentralGraded
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.CenterInvolutions
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.CenterHomocyclic
 import OddOrder.Peterfalvi.Appendices.Suzuki2Groups.HigmanDE
