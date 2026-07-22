bfe41dfe287c5b55dae7d15b203d60b64a9abd7b
Yawara ISHIDA <ni732h.sphl@gmail.com>
2026-07-23 08:52:22 +0900

    refactor(9322): commutator-sup modulo normal を共有化

--- OddOrder.lean
+++ OddOrder.lean
@@ -33,6 +33,7 @@ import OddOrder.GroupTheory.ElementaryAbelianFamily
 import OddOrder.GroupTheory.FittingHeredity
 import OddOrder.GroupTheory.NormalHallHeredity
 import OddOrder.GroupTheory.FrattiniPGroup
+import OddOrder.GroupTheory.CommutatorSup
 import OddOrder.GroupTheory.HallCollection
 import OddOrder.GroupTheory.FormalCommutator
 import OddOrder.GroupTheory.FormalCollection
